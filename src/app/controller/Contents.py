6bf7fb2cdd801126a84fa594a9b2f4725ade5308
tanukichi <task320@gmail.com>
2019-05-03 16:31:57 +0900

    記事表示異常系処理追加
    クエリストリングによる異常系処理を追加

--- src/app/controller/Contents.py
+++ src/app/controller/Contents.py
@@ -3,55 +3,64 @@ Created on 2018/11/18
 
 @author: Tanuki
 '''
-from flask import Response as f_response, request, json
+from flask import Response as f_response, request, redirect, url_for
 from app.dto.Response import Response
 from app.accessor.Contents import Contents as dao_contents
 from app.processor import CreateResponseData
 from app.processor.HtmlRender import HtmlRender
 from app.constant.Url import Url
+from app.processor.ConvertType import ConvertType
 
 
 class Contents():
     @classmethod      
     def get(cls, proc):
         if(proc == Url.CONTENTS_ONE):
-            contents_id = request.args.get('contents_id')
-            return cls.contents_one(contents_id)
+            arg_contents_id = request.args.get('contents_id')
+            return cls.contents_one(arg_contents_id)
         elif(proc == Url.CONTENTS_ALL):
-            current_page = int(request.args.get('page'), base=0)
-            return cls.contents_all(current_page)
+            arg_current_page = request.args.get('page')
+            return cls.contents_all(arg_current_page)
         else:
             return "ERRER",500
 
     @staticmethod
-    def contents_one(contents_id):        
-        dao = dao_contents()
-        if(dao.selectContent(contents_id)):
-            return f_response(
-                HtmlRender.render('index.html', CreateResponseData.create_respone_content_data(dao.get_target_content())),
-                mimetype='text/html',
-                content_type='text/html',
-                status=200
-            )
-        else:
-            return "ERRER",500
+    def contents_one(arg_contents_id):                
+        if ConvertType.possibleConvertStringToInt(arg_contents_id):
+            dao = dao_contents()
+            if dao.selectContent(ConvertType.convertStringToInt(arg_contents_id)):
+                params = CreateResponseData.create_respone_content_data(dao.get_target_content())
+                return f_response(
+                    HtmlRender.render('index.html', params),
+                    mimetype='text/html',
+                    content_type='text/html',
+                    status=200
+                )
+        
+        return f_response(
+            HtmlRender.render('nothing_content.html', None),
+            mimetype='text/html',
+            content_type='text/html',
+            status=200
+        )
 
     @staticmethod
-    def contents_all(current_page):
-        page_zero_base = current_page - 1
-        if(page_zero_base < 0):
-            return "ERROR",500
-
-        dao = dao_contents()
-        if(dao.countContents() & dao.selectContentAll(page_zero_base)):
-            return f_response(
-                HtmlRender.render('index.html', CreateResponseData.create_respone_contents_data(current_page, dao.get_count(), dao.get_target_content())),
-                mimetype='text/html',
-                content_type='text/html',
-                status=200
-            )
-        else:
-            return "ERRER",500 
+    def contents_all(arg_current_page):
+        if(ConvertType.possibleConvertStringToInt(arg_current_page)):
+            current_page = ConvertType.convertStringToInt(arg_current_page)
+            page_zero_base = current_page - 1
+            if not page_zero_base < 0:
+                dao = dao_contents()
+                if(dao.countContents() & dao.selectContentAll(page_zero_base)):
+                    params = CreateResponseData.create_respone_contents_data(current_page, dao.get_count(), dao.get_target_content())
+                    return f_response(
+                        HtmlRender.render('index.html', params),
+                        mimetype='text/html',
+                        content_type='text/html',
+                        status=200
+                    )
+        
+        return redirect(url_for('router.get_all', proc='all', page=1))
 
     
         
