a6715b502e2034cbbafeffdf21ecd103bafb5665
tanukichi <task320@gmail.com>
2019-10-14 21:56:32 +0900

    commit Config.py

---
+++ src/Config .py
@@ -0,0 +1,19 @@
+'''
+Created on 2018/11/21
+
+@author: Tanuki
+'''
+import os
+
+class Config(object):
+    APP_NAME = 'Prome'
+    DEBUG = True
+    SQLALCHEMY_ECHO = True
+    DATABASE_URI =  os.getenv('DATABASE_URI')
+    DISPLAY_NUMBER_OF_CONTENTS = 5
+
+    TWITTER_ACCOUNT = 'task320'
+    TWITTER_CARD_IMAGE = 'https://storage.googleapis.com/prome-238513.appspot.com/twitter_card_image.png'
+
+        
+        
